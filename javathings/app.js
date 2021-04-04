debd954f679ee4ea2addcf017786e9aeeace6b27
abdallakhattab <khattababdalla@yahoo.com>
2021-04-04 15:10:55 +0300

    just made the first lab

---
+++ javathings/app.js
@@ -0,0 +1,10 @@
+let pharmasist = prompt('are you a pharmasist?');
+let username = prompt('whats your name?');
+let age = prompt('your age ??'); 
+let pets = prompt("what kind of pets do you love");
+if (pharmasist === 'yes') { alert('welcome Ph. ' + username + ' your age is ' + age + ' its amazing that you love ' + pets) } 
+else { alert(' welcome ' + username + ' your age is ' + age + ' its amazing that you love ' + pets) };
+console.log(typeof pharmasist , pharmasist);
+console.log(typeof username ,username );
+console.log(typeof age , age);
+console.log(typeof pets, pets);
